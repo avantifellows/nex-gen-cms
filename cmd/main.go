d4e70737964c71548891dbac70ee01db8c85c783
ankurcdoshi <ankurcdoshi@yahoo.com>
2024-10-29 14:26:11 +0530

    Test cases for main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/avantifellows/nex-gen-cms/config"
+	"github.com/avantifellows/nex-gen-cms/di"
 	"github.com/avantifellows/nex-gen-cms/internal/constants"
 	"github.com/avantifellows/nex-gen-cms/internal/handlers"
-	"github.com/avantifellows/nex-gen-cms/internal/models"
-	local_repo "github.com/avantifellows/nex-gen-cms/internal/repositories/local"
-	remote_repo "github.com/avantifellows/nex-gen-cms/internal/repositories/remote"
-	"github.com/avantifellows/nex-gen-cms/internal/services"
 )
 
 func main() {
 	// New mux object is created here instead of using Default via http, so that we can create its mock in testing
 	mux := http.NewServeMux()
-	setup(new(Config), mux)
+	appComponentPtr, err := di.NewAppComponent()
+	if err != nil {
+		panic(err)
+	}
+
+	setup(new(Config), mux, appComponentPtr)
 	http.ListenAndServe(":8080", mux)
 }
 
@@ -37,14 +38,14 @@ type MuxHandler interface {
 	HandleFunc(string, func(http.ResponseWriter, *http.Request))
 }
 
-func setup(configLoader ConfigLoader, muxHandler MuxHandler) {
+func setup(configLoader ConfigLoader, muxHandler MuxHandler, appComponentPtr *di.AppComponent) {
 	constants.InitRuntimeConstant()
 
 	// Load environment variables
 	configLoader.LoadEnv(new(config.Env))
 
 	// this is for output.css file used in home.html
-	muxHandler.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("./web"))))
+	muxHandler.Handle("/web/", appComponentPtr.CssPathHandler)
 
 	muxHandler.HandleFunc("/", handlers.GenericHandler)
 	muxHandler.HandleFunc("/modules", handlers.GenericHandler)
@@ -52,28 +53,11 @@ func setup(configLoader ConfigLoader, muxHandler MuxHandler) {
 	muxHandler.HandleFunc("/major-tests", handlers.GenericHandler)
 	muxHandler.HandleFunc("/add-chapter", handlers.GenericHandler)
 
-	// Initialize repositories
-	cacheRepo := local_repo.NewCacheRepository(5*time.Minute, 10*time.Minute)
-	apiRepo := remote_repo.NewAPIRepository()
-
-	// Initialize service
-	topicsService := services.NewService[models.Topic](cacheRepo, apiRepo)
-	chaptersService := services.NewChapterService(cacheRepo, apiRepo)
-	curriculumsService := services.NewService[models.Curriculum](cacheRepo, apiRepo)
-	gradesService := services.NewService[models.Grade](cacheRepo, apiRepo)
-	subjectsService := services.NewService[models.Subject](cacheRepo, apiRepo)
-
-	// Initialize handlers
-	// topicsHandler := handlers.NewTopicsHandler(topicsService)
-	chaptersHandler := handlers.NewChaptersHandler(chaptersService, topicsService)
-	curriculumsHandler := handlers.NewCurriculumsHandler(curriculumsService)
-	gradesHandler := handlers.NewGradesHandler(gradesService)
-	subjectsHandler := handlers.NewSubjectsHandler(subjectsService)
-
+	chaptersHandler := appComponentPtr.ChaptersHandler
 	muxHandler.HandleFunc("/chapters", chaptersHandler.LoadChapters)
-	muxHandler.HandleFunc("/api/curriculums", curriculumsHandler.GetCurriculums)
-	muxHandler.HandleFunc("/api/grades", gradesHandler.GetGrades)
-	muxHandler.HandleFunc("/api/subjects", subjectsHandler.GetSubjects)
+	muxHandler.HandleFunc("/api/curriculums", appComponentPtr.CurriculumsHandler.GetCurriculums)
+	muxHandler.HandleFunc("/api/grades", appComponentPtr.GradesHandler.GetGrades)
+	muxHandler.HandleFunc("/api/subjects", appComponentPtr.SubjectsHandler.GetSubjects)
 	muxHandler.HandleFunc("/api/chapters", chaptersHandler.GetChapters)
 	muxHandler.HandleFunc("/edit-chapter", chaptersHandler.EditChapter)
 	muxHandler.HandleFunc("/update-chapter", chaptersHandler.UpdateChapter)
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"net/http"
+	"reflect"
 	"testing"
 
 	"github.com/avantifellows/nex-gen-cms/config"
+	"github.com/avantifellows/nex-gen-cms/di"
+	"github.com/avantifellows/nex-gen-cms/internal/constants"
+	"github.com/avantifellows/nex-gen-cms/internal/handlers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -18,23 +22,30 @@ func (m *MockConfig) LoadEnv(loader config.EnvLoader) {
 }
 
 type MockServeMux struct {
-	mux    *http.ServeMux
-	routes map[string]http.HandlerFunc
+	mux           *http.ServeMux
+	routeHandlers map[string]http.Handler
+	/**
+	  following extra attribute would be required if we don't want to convert handlerFunc parameter
+	  to HandlerFunc type in HandleFunc() implemented below for MockServeMux. In that case all expected values
+	  & verifications will also be separate in the same way as done for routeHandlers
+	*/
+	// routeHandlerFuncs map[string]http.HandlerFunc
 }
 
 func NewMockServeMux() *MockServeMux {
 	return &MockServeMux{
-		mux:    http.NewServeMux(),
-		routes: make(map[string]http.HandlerFunc),
+		mux:           http.NewServeMux(),
+		routeHandlers: make(map[string]http.Handler),
 	}
 }
 
 func (m *MockServeMux) Handle(pattern string, handler http.Handler) {
+	m.routeHandlers[pattern] = handler
 	m.mux.Handle(pattern, handler)
 }
 
 func (m *MockServeMux) HandleFunc(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
-	m.routes[pattern] = handlerFunc
+	m.routeHandlers[pattern] = http.HandlerFunc(handlerFunc)
 	m.mux.HandleFunc(pattern, handlerFunc)
 }
 
@@ -44,23 +55,50 @@ func TestSetup(t *testing.T) {
 
 	// Create a new MockServeMux to capture registered routes
 	mockServeMux := NewMockServeMux()
-	expectedRoutes := []string{
-		"/", "/modules", "/books", "/major-tests", "/add-chapter",
-		"/chapters", "/api/curriculums", "/api/grades", "/api/subjects",
-		"/api/chapters", "/edit-chapter", "/update-chapter", "/create-chapter", "/delete-chapter",
+	appComponentPtr, _ := di.NewAppComponent()
+	chaptersHandler := appComponentPtr.ChaptersHandler
+
+	expectedRouteHandlers := []struct {
+		pattern string
+		handler http.Handler
+	}{
+		{"/web/", appComponentPtr.CssPathHandler},
+		{"/", http.HandlerFunc(handlers.GenericHandler)},
+		{"/modules", http.HandlerFunc(handlers.GenericHandler)},
+		{"/books", http.HandlerFunc(handlers.GenericHandler)},
+		{"/major-tests", http.HandlerFunc(handlers.GenericHandler)},
+		{"/add-chapter", http.HandlerFunc(handlers.GenericHandler)},
+		{"/chapters", http.HandlerFunc(chaptersHandler.LoadChapters)},
+		{"/api/curriculums", http.HandlerFunc(appComponentPtr.CurriculumsHandler.GetCurriculums)},
+		{"/api/grades", http.HandlerFunc(appComponentPtr.GradesHandler.GetGrades)},
+		{"/api/subjects", http.HandlerFunc(appComponentPtr.SubjectsHandler.GetSubjects)},
+		{"/api/chapters", http.HandlerFunc(chaptersHandler.GetChapters)},
+		{"/edit-chapter", http.HandlerFunc(chaptersHandler.EditChapter)},
+		{"/update-chapter", http.HandlerFunc(chaptersHandler.UpdateChapter)},
+		{"/create-chapter", http.HandlerFunc(chaptersHandler.AddChapter)},
+		{"/delete-chapter", http.HandlerFunc(chaptersHandler.DeleteChapter)},
 	}
 
-	setup(mockConfig, mockServeMux)
+	setup(mockConfig, mockServeMux, appComponentPtr)
 
+	// verify if runtime constants are  initialized
+	assert.NotEmpty(t, constants.GetHtmlFolderPath(), "Runtime constants are not initilized")
+
+	// verify if environment variables are loaded from .env file
 	mockConfig.AssertCalled(t, "LoadEnv", mock.Anything)
 
-	registeredRoutes := mockServeMux.routes
-	// Assert that all expected routes are registered
-	for _, route := range expectedRoutes {
-		_, ok := registeredRoutes[route]
-		assert.True(t, ok, "Route not registered: "+route)
+	// Assert that expected route handlers are registered
+	registeredRouteHandlers := mockServeMux.routeHandlers
+	for _, expectedRH := range expectedRouteHandlers {
+		pattern := expectedRH.pattern
+		registeredHandler, ok := registeredRouteHandlers[pattern]
+		assert.True(t, ok, "Route not registered: "+pattern)
+		assert.True(t, areHandlersEqual(registeredHandler, expectedRH.handler), "Handler mismatch for pattern %s", pattern)
 	}
+	assert.Equal(t, len(expectedRouteHandlers), len(registeredRouteHandlers), "Unexpected number of routes registered")
+}
 
-	// Assert the number of registered routes
-	assert.Equal(t, len(expectedRoutes), len(registeredRoutes), "Unexpected number of routes registered")
+// Function to compare function addresses using reflect (Using reflect because functions cannot be compared otherwise)
+func areHandlersEqual(h1, h2 http.Handler) bool {
+	return reflect.ValueOf(h1).Pointer() == reflect.ValueOf(h2).Pointer()
 }
