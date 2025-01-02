ededf91e9927e1f22cfc6afccd7c5eb08e5e5493
ankurcdoshi <ankurcdoshi@yahoo.com>
2025-01-02 18:59:15 +0530

    Terraform and CI/CD files

---
+++ infra/variables.tf
@@ -0,0 +1,27 @@
+variable "instance_type" {
+  description = "The type of EC2 instance to use"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_api_token" {
+  description = "API token for managing DNS in Cloudflare"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+}
+
