8ddf1e7d50f208f0e814a0d9d87dbf406c5dfd7a
ankurcdoshi <ankurcdoshi@yahoo.com>
2026-01-14 18:27:15 +0530

    Removed window.MathJax condition in mathjax script waiting logic

--- internal/handlers/test_handler.go
+++ internal/handlers/test_handler.go
@@ -840,8 +840,6 @@ func (h *TestsHandler) DownloadPdf(responseWriter http.ResponseWriter, request *
                     const script = document.querySelector('script[id="MathJax-script"]');
                     if (script && (script.readyState === 'complete' || script.readyState === 'loaded')) {
                         resolve(true);
-                    } else if (window.MathJax) {
-                        resolve(true);
                     } else {
                         setTimeout(waitForScript, 100);
                     }
