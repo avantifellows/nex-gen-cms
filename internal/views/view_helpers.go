5ac2ce3bcc19aca34b42195a29c12ae785f7ead7
ankurcdoshi <ankurcdoshi@yahoo.com>
2026-04-07 14:59:45 +0530

    Merge conflict resolved

--- internal/views/view_helpers.go
+++ internal/views/view_helpers.go
@@ -8,6 +8,8 @@ func GetSectionName(defaultType string, customName string) string {
 	switch defaultType {
 	case "mcq_single_answer":
 		return "MCQ Single Answer"
+	case "mcq_multiple_answer":
+		return "MCQ Multiple Answer"
 	case "numerical_answer":
 		return "Numerical Answer"
 	case "integer_type":
