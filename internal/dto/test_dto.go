aa7baa07f16e15f41416eef831c60bee0650ac17
ankurcdoshi <ankurcdoshi@yahoo.com>
2026-04-21 00:17:03 +0530

    Conflicts resolved

--- internal/dto/test_dto.go
+++ internal/dto/test_dto.go
@@ -7,4 +7,6 @@ type TestData struct {
 	TestPtr  *models.Test
 	Problems map[int]*models.Problem // key = Problem ID
 	TestRule *models.TestRule
+	// True when the test's exam is JEE Advanced (matrix match gets its own section on add-test).
+	IsJeeAdvancedExam bool `json:"IsJeeAdvancedExam"`
 }
